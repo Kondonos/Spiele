81d2e4f6450a7eb781faf7d8f1d71b9bd6824665
Kondonos <mhenschell@googlemail.com>
2015-11-03 22:53:39 +0100

    divide item into item and itemstack, item has no effects

---
+++ Aufriss-Base/src/item/Itemstack.java
@@ -0,0 +1,65 @@
+package item;
+
+public class Itemstack {
+	Item item;
+	int max;
+	int current;
+	
+	public Itemstack(Item item, int max, int current) {
+		this.item=item;
+		this.max=max;
+		this.current=current;
+	}
+	public int additem(Itemstack itemstack){
+		if(this.item.equals(itemstack.item)){
+			this.current+=itemstack.getCurrent();
+			if(this.current>=this.max){
+				int rest=current%max;
+				current=max;
+				return rest;
+			}else return 0;
+		}else return itemstack.getCurrent();
+	}
+	public int additem(int count){
+		this.current+=count;
+		if(this.current>=this.max){
+			int rest=current%max;
+			current=max;
+			return rest;
+		}else return 0;
+	}
+	public int subitem(int count) throws Throwable{
+		this.current-=count;
+		if(this.current<=0){
+			this.finalize();
+			return -1*this.current;
+		}else return 0;	
+	}
+	@Override
+	protected void finalize() throws Throwable {
+		this.item=null;
+		this.current=0;
+		this.max=0;
+		super.finalize();
+	}
+	
+	
+	public int getCurrent() {
+		return current;
+	}
+	public Item getItem() {
+		return item;
+	}
+	public int getMax() {
+		return max;
+	}
+	public void setCurrent(int current) {
+		this.current = current;
+	}
+	public void setItem(Item item) {
+		this.item = item;
+	}
+	public void setMax(int max) {
+		this.max = max;
+	}
+}
