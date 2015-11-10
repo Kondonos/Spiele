df2c099d6ed226738567869f0e759e6e3b59e510
Kondonos <mhenschell@googlemail.com>
2015-11-10 19:57:02 +0100

    key keys

---
+++ Jp/src/keyboard/Key.java
@@ -0,0 +1,12 @@
+package keyboard;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+
+public class Key {
+	private String letter;
+	
+	public Key(String letter) {
+		this.letter=letter;
+	}
+}
